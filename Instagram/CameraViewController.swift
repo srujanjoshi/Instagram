8f7f1eeda6ee1b53a226790c692b01e3607dbf13
srujanjoshi <srujanjoshi7@gmail.com>
2022-03-24 19:59:12 -0400

    Image gets posted to Server

--- Instagram/CameraViewController.swift
+++ Instagram/CameraViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -19,7 +20,26 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        post["caption"] = commentField.text!
+        post["author"] = PFUser.current()!
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { (success, error) in
+            if success{
+                self.dismiss(animated: true, completion: nil)
+                print("Saved!")
+            } else {
+                print("Error!")
+            }
+        }
+        
     }
+    
     
     
     @IBAction func onCameraButton(_ sender: Any) {
